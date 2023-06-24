55a7b0279b462328dfef5885a580e50f22b7e6dc
Marvin Ruciński <marvin@prasa-polska.com>
2023-06-24 20:49:47 +0200

    updated error pages and conf

--- vue.sh
+++ vue.sh
@@ -1,4 +1,10 @@
 cd vue
 export VITE_API_URL='http://localhost:8000/api/'
+
+# admin
 export VITE_AUTH_HEADER='{ "Authorization": "Token 67bdbcffd9c883b098da608f57db393c868ef178" }'
+
+# test user
+# export VITE_AUTH_HEADER='{ "Authorization": "Token bb2d326dbebc26ac0dec55e96acbb02df83acbba" }'
+
 npm run dev

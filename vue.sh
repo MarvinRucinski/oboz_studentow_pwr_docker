08137c1e8df2277db54fa4b81f37dca20b9b7081
Marvin Ruciński <marvin@prasa-polska.com>
2023-07-24 13:06:21 +0200

    new settings

--- vue.sh
+++ vue.sh
@@ -1,5 +1,6 @@
 cd vue
-export VITE_API_URL='http://192.168.0.34:8000/api/'
+# export VITE_API_URL='http://192.168.1.150:8000/api/'
+export VITE_API_URL='http://localhost:8000/api/'
 
 # admin
 export VITE_AUTH_HEADER='{ "Authorization": "Token REMOVED" }'

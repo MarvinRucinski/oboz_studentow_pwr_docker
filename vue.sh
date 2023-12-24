79dcf88af987e365ccedd24556dbacf93bb580d9
Marvin Ruciński <marvin@prasa-polska.com>
2023-12-24 16:50:33 +0100

    Updated submodules; small changes

--- vue.sh
+++ vue.sh
@@ -3,6 +3,8 @@ export VITE_API_URL='http://localhost:8000/api/'
 export VITE_WS_API_URL='ws://localhost:8000/'
 # export VITE_API_URL='http://192.168.0.34:8000/api/'
 # export VITE_WS_API_URL='ws://192.168.0.34:8000/'
+# export VITE_API_URL='http://172.20.10.2:8000/api/'
+# export VITE_WS_API_URL='ws://172.20.10.2:8000/'
 
 
 

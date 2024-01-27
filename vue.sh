b2bd063327e362e289aff8f01352c2b04fd30cb3
Marvin Ruciński <marvin@prasa-polska.com>
2024-01-27 20:43:02 +0100

    Added redis, fixed vue env variables, disabled SSL in nginx

--- vue.sh
+++ vue.sh
@@ -1,15 +1,16 @@
 cd vue
 export VITE_API_URL='http://localhost:8000/api/'
 export VITE_WS_API_URL='ws://localhost:8000/'
-# export VITE_API_URL='http://192.168.0.34:8000/api/'
-# export VITE_WS_API_URL='ws://192.168.0.34:8000/'
+# export VITE_API_URL='http://192.168.1.102:8000/api/'
+# export VITE_WS_API_URL='ws://192.168.1.102:8000/'
 # export VITE_API_URL='http://172.20.10.2:8000/api/'
 # export VITE_WS_API_URL='ws://172.20.10.2:8000/'
 
 
 
 # admin
-export VITE_AUTH_HEADER='{ "Authorization": "Token REMOVED" }'
+# export VITE_AUTH_HEADER='{ "Authorization": "Token REMOVED" }'
+export VITE_AUTH_TOKEN='REMOVED'
 
 # test user
 # export VITE_AUTH_HEADER='{ "Authorization": "Token bb2d326dbebc26ac0dec55e96acbb02df83acbba" }'

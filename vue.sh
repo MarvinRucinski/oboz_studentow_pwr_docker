c179c1e1909cec7e74584f12161ad3b897a865c5
Marvin Ruciński <marvin@prasa-polska.com>
2024-01-28 16:53:05 +0100

    changed remianing colors

--- vue.sh
+++ vue.sh
@@ -1,8 +1,8 @@
 cd vue
-export VITE_API_URL='http://localhost:8000/api/'
-export VITE_WS_API_URL='ws://localhost:8000/'
-# export VITE_API_URL='http://192.168.1.102:8000/api/'
-# export VITE_WS_API_URL='ws://192.168.1.102:8000/'
+# export VITE_API_URL='http://localhost:8000/api/'
+# export VITE_WS_API_URL='ws://localhost:8000/'
+export VITE_API_URL='http://192.168.1.143:8000/api/'
+export VITE_WS_API_URL='ws://192.168.1.143:8000/'
 # export VITE_API_URL='http://172.20.10.2:8000/api/'
 # export VITE_WS_API_URL='ws://172.20.10.2:8000/'
 

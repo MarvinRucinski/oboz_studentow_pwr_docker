b89e88fdd16840f2a16acc4ee53b85081ecd34d3
Marvin Ruciński <marvin@prasa-polska.com>
2024-03-17 17:14:24 +0100

    Changed back to VITE

--- vue.sh
+++ vue.sh
@@ -1,19 +1,19 @@
 cd vue
-# export VUE_APP_API_URL='http://localhost:8000/api/'
-# export VUE_APP_WS_API_URL='ws://localhost:8000/'
-# export VUE_APP_API_URL='http://192.168.0.186:8000/api/'
-# export VUE_APP_WS_API_URL='ws://192.168.0.186:8000/'
-# export VUE_APP_API_URL='http://172.20.10.2:8000/api/'
-# export VUE_APP_WS_API_URL='ws://172.20.10.2:8000/'
+# export VITE_API_URL='http://localhost:8000/api/'
+# export VITE_WS_API_URL='ws://localhost:8000/'
+# export VITE_API_URL='http://192.168.0.186:8000/api/'
+# export VITE_WS_API_URL='ws://192.168.0.186:8000/'
+# export VITE_API_URL='http://172.20.10.2:8000/api/'
+# export VITE_WS_API_URL='ws://172.20.10.2:8000/'
 
 
 
 # admin
-# export VUE_APP_AUTH_HEADER='{ "Authorization": "Token 67bdbcffd9c883b098da608f57db393c868ef178" }'
-# export VUE_APP_AUTH_TOKEN='67bdbcffd9c883b098da608f57db393c868ef178'
+# export VITE_AUTH_HEADER='{ "Authorization": "Token 67bdbcffd9c883b098da608f57db393c868ef178" }'
+# export VITE_AUTH_TOKEN='67bdbcffd9c883b098da608f57db393c868ef178'
 
 # test user
-# export VUE_APP_AUTH_HEADER='{ "Authorization": "Token bb2d326dbebc26ac0dec55e96acbb02df83acbba" }'
+# export VITE_AUTH_HEADER='{ "Authorization": "Token bb2d326dbebc26ac0dec55e96acbb02df83acbba" }'
 
 # npx vite --host
 ionic serve --browser chrome --host 0.0.0.0 --force

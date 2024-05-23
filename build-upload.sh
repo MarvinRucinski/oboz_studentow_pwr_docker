5b629f096a8e53aacd6d92bd2908d18ff67d2b28
Marvin Ruciński <marvin@prasa-polska.com>
2024-05-23 13:13:57 +0200

    updates in docker config

--- build-upload.sh
+++ build-upload.sh
@@ -1,1 +1,1 @@
-docker -H ssh://marvinr@188.68.247.148:22 compose -f docker-compose.yml -f production.yml up -d --build
+docker -H ssh://dockeruser@156.17.197.71 compose -f docker-compose.yml -f production.yml up -d --build

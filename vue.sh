c0bef8cbf618952593f9f0c4bab4baeea2ca108e
Marvin Ruciński <marvin@prasa-polska.com>
2024-06-25 16:00:48 +0200

    config updates

--- vue.sh
+++ vue.sh
@@ -5,8 +5,8 @@ cd vue
 # export VITE_WS_API_URL='ws://192.168.0.186:8000/'
 # export VITE_API_URL='http://172.20.10.2:8000/api/'
 # export VITE_WS_API_URL='ws://172.20.10.2:8000/'
-export VITE_API_URL='https://www.api.oboz.samorzad.pwr.edu.pl/api/'
-export VITE_WS_API_URL='wss://www.api.oboz.samorzad.pwr.edu.pl/'
+# export VITE_API_URL='https://www.api.oboz.samorzad.pwr.edu.pl/api/'
+# export VITE_WS_API_URL='wss://www.api.oboz.samorzad.pwr.edu.pl/'
 
 
 # npx vite --host
